eca015fa02d26fe8db7ae2880e2192d4f32dc571
Ben Hallion <bhallion@blackducksoftware.com>
2017-07-23 04:27:31 -0400

    Use applescript to launch terminals

--- hub-dev.js
+++ hub-dev.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const execute = require('./lib/execute');
+const escapeString = require('escape-string-applescript');
 
 const hubBuildPath = path.resolve(__dirname, 'commands/hub-start.command');
 const uiBuildPath = path.resolve(__dirname, 'commands/ui-build.command');
 const proxyPath = path.resolve(__dirname, 'commands/ui-proxy.command');
 
 Promise.all([
-    execute(`open ${hubBuildPath}`),
-    execute(`open ${uiBuildPath}`),
-    execute(`open ${proxyPath}`)
+    execute(`osascript -e 'tell application "Terminal" to do script "${escapeString(hubBuildPath)}"'`),
+    execute(`osascript -e 'tell application "Terminal" to do script "${escapeString(uiBuildPath)}"'`),
+    execute(`osascript -e 'tell application "Terminal" to do script "${escapeString(proxyPath)}"'`)
 ]);
