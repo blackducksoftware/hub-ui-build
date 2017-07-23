288dee49d6a0a1a6aca6eb62970f50b3da5b8f4a
Ben Hallion <bhallion@blackducksoftware.com>
2017-07-23 03:06:18 -0400

    Position terminals

--- bash-aliases.js
+++ bash-aliases.js
@@ -9,14 +9,15 @@ if (os.platform() === 'win32') {
 
 const runHubPath = path.resolve(__dirname, 'hub-up.js');
 const runDevPath = path.resolve(__dirname, 'hub-dev.js');
-const bashrcPath = path.resolve(os.homedir(), '.bash_profile');
+const bashrcPath = path.resolve(os.homedir(), '.bashrc');
+const bashProfilePath = path.resolve(os.homedir(), '.bash_profile');
 const uiDir = path.resolve(__dirname, '../ui');
 const cmdsDir = path.resolve(__dirname, 'commands');
-const startHub = `node ${runHubPath};`;
+const startHub = `node ${runHubPath}`;
 
 const aliases = [
-    `alias hub-up='${startHub}'`,
-    `alias hub-dev='node ${runDevPath}'`
+    `alias hub-up='${startHub};'`,
+    `alias hub-dev='node ${runDevPath};'`
 ];
 
 fsProm.isFile(bashrcPath)
@@ -29,8 +30,20 @@ fsProm.isFile(bashrcPath)
         }
     });
 
-const buildUi = `cd ${uiDir};\ngrunt default watch;\n`;
-const runLocalProxy = `cd ${uiDir};\nnode dev-server.js --local-port=8081 --remote-port=8080 --remote-host=localhost --no-mocks --no-ssl;\n`;
+const source = `source ${bashrcPath}`;
+
+fsProm.isFile(bashProfilePath)
+    .catch(err => false)
+    .then(isFile => {
+        if (isFile) {
+            return fsProm.concatUniqueLines(bashProfilePath, [source]);
+        } else {
+            return fsProm.writeFile(bashProfilePath, `${source}\n`);
+        }
+    });
+
+const buildUi = `printf '\\e[4;480;470t'; printf '\\e[3;540;0t'; printf '\\e[5t'; cd ${uiDir};\ngrunt default watch;\n`;
+const runLocalProxy = `printf '\\e[4;480;470t'; printf '\\e[3;540;470t'; cd ${uiDir};\nnode dev-server.js --local-port=8081 --remote-port=8080 --remote-host=localhost --no-mocks --no-ssl;\n`;
 const buildUiCmdPath = path.resolve(cmdsDir, 'ui-build.command');
 const buildHubCmdPath = path.resolve(cmdsDir, 'hub-start.command');
 const proxyCmdPath = path.resolve(cmdsDir, 'ui-proxy.command');
@@ -38,6 +51,11 @@ const proxyCmdPath = path.resolve(cmdsDir, 'ui-proxy.command');
 fsProm.cleanDirectory(cmdsDir)
     .then(() => Promise.all([
         fsProm.writeFile(buildUiCmdPath, buildUi),
-        fsProm.writeFile(buildHubCmdPath, startHub),
+        fsProm.writeFile(buildHubCmdPath, `printf '\\e[4;0;540t'; printf '\\e[3;0;0t'; ${startHub};`),
         fsProm.writeFile(proxyCmdPath, runLocalProxy)
+    ]))
+    .then(() => Promise.all([
+        execute.setPermission(buildUiCmdPath),
+        execute.setPermission(buildHubCmdPath),
+        execute.setPermission(proxyCmdPath)
     ]));
--- hub-dev.js
+++ hub-dev.js
@@ -6,12 +6,7 @@ const uiBuildPath = path.resolve(__dirname, 'commands/ui-build.command');
 const proxyPath = path.resolve(__dirname, 'commands/ui-proxy.command');
 
 Promise.all([
-        execute.setPermission(uiBuildPath),
-        execute.setPermission(hubBuildPath),
-        execute.setPermission(proxyPath)
-    ])
-    .then(() => Promise.all([
-        execute(`open ${hubBuildPath}`),
-        execute(`open ${uiBuildPath}`),
-        execute(`open ${proxyPath}`)
-    ]));
+    execute(`open ${hubBuildPath}`),
+    execute(`open ${uiBuildPath}`),
+    execute(`open ${proxyPath}`)
+]);
