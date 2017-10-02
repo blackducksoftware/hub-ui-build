4de6a1a7002d39f295e3c08d5f635acd3a349bfa
Ben Hallion <bhallion@blackducksoftware.com>
2017-10-02 02:49:42 -0400

    Validate configuration

--- index.js
+++ index.js
@@ -3,26 +3,33 @@ const fs = require('./lib/fs-promise');
 const path = require('path');
 const os = require('os');
 
-if (os.platform() === 'win32') {
-    throw new Error('We don\'t support aliases for Windows');
+if (os.platform() !== 'darwin') {
+    throw new Error('We only support aliases for MacOS');
 }
+
+const prompt = {
+    filter: (dir) => path.resolve(__dirname, dir),
+    validate: (dir) => {
+        return fs.isDirectory(dir)
+            .then(isDirectory => isDirectory || 'This isn\'t the directory you\'re looking forward. Try again')
+    }
+};
 
 const writeEnvConfig = () => {
     return inquirer.prompt([
-        {
+        Object.assign({
             message: 'What\'s the resolved or relative path to the rest-backend directory?',
             name: 'HUB_REPO_DIR',
-            default: path.join(__dirname, '../rest-backend'),
-            filter: (dir) => path.resolve(__dirname, dir)
-        },
-        {
+            default: path.join(__dirname, '../rest-backend')
+        }, prompt),
+        Object.assign({
             message: 'Same question, for the UI directory?',
             name: 'UI_REPO_DIR',
             default: path.join(__dirname, '../ui'),
-            filter: (dir) => path.resolve(__dirname, dir)
-        }
+        }, prompt)
     ])
         .then((config) => {
+            process.stdout.write('\n');
             return fs.writeFile(
                 path.resolve(__dirname, '.env'),
                 Object.keys(config)
@@ -33,18 +40,14 @@ const writeEnvConfig = () => {
 };
 
 const loadEnvConfig = () => {
-    try {
-        require('dotenv-safe').load({
-            path: path.resolve(__dirname, '.env'),
-            sample: path.resolve(__dirname, '.env.example')
-        });
-    } catch (err) {
-        console.error(err.toString() + '\n');
-        return writeEnvConfig();
-    }
+    require('dotenv-safe').load({
+        path: path.resolve(__dirname, '.env'),
+        sample: path.resolve(__dirname, '.env.example')
+    });
 };
 
 fs.stat('./.env')
     .catch(writeEnvConfig)
     .then(loadEnvConfig)
-    .then(() => require('./bash-aliases'));
+    .then(() => require('./bash-aliases'))
+    .catch((err) => console.error(err));
